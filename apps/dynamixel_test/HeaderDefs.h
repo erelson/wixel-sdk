fe6f5d100c9802d0bee1d6224f47a6598e7224db
Eric Relson <gertlex@gmail.com>
2012-07-27 17:05:04 -0500

    Copied wireless_serial_twitch to dynamixel_test; removed usb/wireless code.

---
+++ apps/dynamixel_test/HeaderDefs.h
@@ -0,0 +1,318 @@
+///%s Expects a 'char *', 'unsigned char*' or 'uint8_t *'
+///%d Expects an 'int', 'int8_t' or an 'int16_t'
+///%ld Expects a 'long' or 'int32_t'
+///%u Expects an 'uint8_t' or an 'uint16_t'
+///%lu Expects an 'unsigned long' or 'uint32_t'
+///%o Expects an 'uint8_t' or an 'uint16_t' and outputs the number in octal (base 8)
+///%lo Expects an 'unsigned long' or 'uint32_t' and outputs the number in octal (base 8)
+///%x Expects an 'uint8_t' or an 'uint16_t' and outputs the number in hexadecimal (base 16)
+///%lx Expects an 'uint32_t' and outputs the number in hexadecimal (base 16)
+///%% Expects no parameter and just outputs the '%' character
+///%c Expects a 'char', 'signed char', 'int8_t', 'int16_t', 'int'
+///%f Expects a 'float' or 'double'
+
+#include <wixel.h>
+
+// defines for turning various bits of output on/off
+#define PRINT_IR_RANGE 0
+#define PRINT_DEBUG_COMMANDER 1
+#define PRINT_DEBUG_IK 0
+#define PRINT_DEBUG_LOOP 0
+#define PRINT_DEBUG 0
+#define PRINT_DEBUG_AGITATOR 0
+
+#define RTN_LVL 1
+#define MY_SERVO_SPEED 300
+
+#define zTRUE 1
+#define zFALSE 0
+
+#define LISTEN UART2toXbee38400
+// #define LISTEN UART0toUSB38400
+
+// short derp;
+
+// extern int do_gait;// = 1;
+
+// extern void initTrig();  //function prototype
+					// //function was in servoPosConsts.h
+
+// extern void initServoLims();  //function prototype
+
+// extern void setAXposition(int16_t, DYNAMIXEL_AX12, int16_t, int16_t);
+// extern void myServoSpeeds(DYNAMIXEL_AX12_LIST*);
+// extern void myServoReturnLevels(DYNAMIXEL_AX12_LIST*);
+// extern void g8Stand();
+// extern void g8FeetDown();
+// extern void g8Flop();
+// extern void walkNewDirIK(int16_t);
+// extern void initTrig();
+// extern void initServoLims();
+// extern void agitatorLogic(TICK_COUNT);
+
+// extern void turnCode(short,int16_t,int16_t);
+// extern void walkCode(int16_t,int16_t,int16_t,int16_t);
+
+// extern TICK_COUNT speedPhaseFix(TICK_COUNT, TICK_COUNT, TICK_COUNT);
+
+/*extern void doLegKinematics(uint16_t, uint16_t*, uint16_t*, uint16_t*, short, \
+ 			float, float, float, float, int16_t, float, short);
+*/
+// extern uint8_t calc_foot_h(int16_t, uint16_t, float, int16_t, float, float);
+
+//Servo position variables; defaulting these to centered...
+// uint32 s11pos;
+// uint32 s12pos;
+// uint32 s13pos;
+// extern uint16 s14pos;
+
+// extern uint16_t s21pos;
+// extern uint16_t s22pos;
+// extern uint16_t s23pos;
+// extern uint16_t s24pos;
+
+// extern uint16_t s31pos;
+// extern uint16_t s32pos;
+// extern uint16_t s33pos;
+// extern uint16_t s34pos;
+
+// extern uint16_t s41pos;
+// extern uint16_t s42pos;
+// extern uint16_t s43pos;
+// extern uint16_t s44pos;
+
+// extern uint16_t s51pos; // added a 45° offset
+// extern uint16_t s52pos;
+
+// //Servo position limits, range from 0 to 1023
+// extern uint16_t servo11Min; 
+// extern uint16_t servo11Max;
+// extern uint16_t servo12Min;
+// extern uint16_t servo12Max;
+// extern uint16_t servo13Min;
+// extern uint16_t servo13Max;
+// extern uint16_t servo14Min;
+// extern uint16_t servo14Max;
+
+// extern uint16_t servo21Min;
+// extern uint16_t servo21Max;
+// extern uint16_t servo22Min;
+// extern uint16_t servo22Max;
+// extern uint16_t servo23Min;
+// extern uint16_t servo23Max;
+// extern uint16_t servo24Min;
+// extern uint16_t servo24Max;
+
+// extern uint16_t servo31Min;
+// extern uint16_t servo31Max;
+// extern uint16_t servo32Min;
+// extern uint16_t servo32Max;
+// extern uint16_t servo33Min;
+// extern uint16_t servo33Max;
+// extern uint16_t servo34Min;
+// extern uint16_t servo34Max;
+
+// extern uint16_t servo41Min;
+// extern uint16_t servo41Max;
+// extern uint16_t servo42Min;
+// extern uint16_t servo42Max;
+// extern uint16_t servo43Min;
+// extern uint16_t servo43Max;
+// extern uint16_t servo44Min;
+// extern uint16_t servo44Max;
+
+// See WalkingOmni.nb
+//Center angles for coax servo of each leg.plus test offsets.
+// Measured from 0deg = front of bot
+#define s11A0 225
+#define s11Aoff 10
+#define s21A0 -45 //315 //Hmmm no idea here....
+#define s21Aoff -10
+#define s31A0 45
+#define s31Aoff 0
+#define s41A0 135
+#define s41Aoff 0
+
+
+#define c_s11A0 cos(s11A0 + s11Aoff)
+#define s_s11A0 sin(s11A0 + s11Aoff)
+#define c_s21A0 cos(s21A0 + s21Aoff)
+#define s_s21A0 sin(s21A0 + s21Aoff)
+#define c_s31A0 cos(s31A0 + s31Aoff)
+#define s_s31A0 sin(s31A0 + s31Aoff)
+#define c_s41A0 cos(s41A0 + s41Aoff)
+#define s_s41A0 sin(s41A0 + s41Aoff)
+
+
+//Center angles for each leg.
+// extern float servo11Ang;
+// extern float servo21Ang;
+// extern float servo31Ang;
+// extern float servo41Ang;
+
+// extern float cos_servo11Ang;
+// extern float sin_servo11Ang;
+// extern float cos_servo21Ang;
+// extern float sin_servo21Ang;
+// extern float cos_servo31Ang;
+// extern float sin_servo31Ang;
+// extern float cos_servo41Ang;
+// extern float sin_servo41Ang;
+
+// // Leg consts
+// extern uint16_t bodyH;
+
+// extern uint16_t L0;
+
+	// //Leg part lengths
+// #define preL12 58
+// #define preL23 63
+// #define preL34 67
+// #define preL45 57
+
+// extern uint16_t y11;
+// extern uint16_t y21;
+// extern uint16_t y31;
+// extern uint16_t y41;
+
+// extern int L12;
+// extern int L23;
+// extern int L34;
+// extern int L45;
+
+// extern int sqL23;
+// extern int sqL34;
+// //not used:
+// //int sqL45 = preL45 * preL45;
+
+// //????????
+// extern int x11;
+// extern int x21;
+// extern int x31;
+// extern int x41;
+
+// //Leg ServoPos Offsets
+// //  this is in units of... 10bit position?
+// //  currently not used... manually entered values are in 
+// extern int offsetServo1;
+// extern int offsetServo2;
+// extern int offsetServo3;
+// extern int offsetServo4;
+
+///////////////////////
+/////TURRET////////////
+///////////////////////
+//+153 is 45 deg offset for pan servo's mounting scheme.
+#define PAN_CENTER 511 + 153 
+#define TILT_CENTER 511 + 95
+
+uint8 d_tilt;
+uint8 d_pan;
+uint8 tilt_pos; // where tilt servo ought to be
+uint8 pan_pos;  // where pan servo ought to be
+
+// // 4*52 -> 60 degrees?
+// extern uint16_t servo51Min;
+// extern uint16_t servo51Max;
+// extern uint16_t servo52Min;
+// extern uint16_t servo52Max;
+
+// extern int servo51Min;
+// extern int servo51Max;
+// extern int servo52Min;
+// extern int servo52Max;
+
+
+
+////////////////////
+/////Timing/////////
+///////////////////
+// extern int16_t now2;
+// extern int16_t now3;
+// extern int16_t now4;
+// extern int16_t now1;
+
+
+// extern TICK_COUNT guns_firing_start_time;
+// extern TICK_COUNT guns_firing_start_time;
+// extern TICK_COUNT guns_firing_duration;
+// extern TICK_COUNT spdChngOffset;
+
+short turnright;
+short turnleft;
+short gunbutton;
+short panicbutton;
+short infobutton;
+short agitbutton;
+
+// extern uint16_t loopLengthList[];
+
+// extern int16_t ang_dir;
+short walk;
+short turn;
+short light;
+short kneeling;
+short flopCnt;
+short panic;
+// extern TICK_COUNT agitate;
+
+uint8 pan_add;
+uint8 tilt_add;
+
+//////////////
+// The below lines are adapted from AX12.h in Webbotlib. Check out Clive Webster's work at http://webbot.org.uk/
+// From Webbotlib: libdefs.h
+// MIN/MAX/ABS macros
+#define MIN(a,b)			(((a)<(b))?(a):(b))
+#define MAX(a,b)			(((a)>(b))?(a):(b))
+#define ABS(x)				(((x)>0)?(x):(-(x)))
+
+// Clamp a value to be in a given range
+#define CLAMP(val, min, max) MAX(MIN(val,max),min)
+
+/*
+*	Interpolate between two numbers
+*	value - the current value to be used
+*   minVal - the minimum that 'value' can be
+*   maxVal - the maximum that 'value' can be
+*   minRtn - the return value if 'value = minVal'
+*   maxRtn - the return value if 'value = maxVal'
+*   return a value in the range minRtn to maxRtn
+*/
+int16 interpolate(int16 value, int16 minVal, int16 maxVal, int16 minRtn, int16 maxRtn);
+
+
+////////////////
+// The former contents of Commander.h
+//#ifndef Commander_h
+//#define Commander_h
+//
+//#define LISTEN UART2toXbee38400
+
+/* bitmasks for buttons array */
+#define BUT_R1      0x01
+#define BUT_R2      0x02
+#define BUT_R3      0x04
+#define BUT_L4      0x08
+#define BUT_L5      0x10
+#define BUT_L6      0x20
+#define BUT_RT      0x40
+#define BUT_LT      0x80
+
+int CmdrReadMsgs();
+void UseSouthPaw();
+
+// variables used for reading messages
+unsigned char vals[7];  // temporary values, moved after we confirm checksum_cmdr
+int index_cmdr;              // -1 = waiting for new packet
+int checksum_cmdr;
+unsigned char southpaw; 
+
+int walkV;
+int walkH;
+int lookV;
+int lookH;
+int walkSPD;
+float walkDIR;
+
+//#endif
---
+++ apps/dynamixel_test/dynamixel_test.c
@@ -0,0 +1,341 @@
+/* wireless_serial app:
+ * This app allows you to connect two Wixels together to make a wireless,
+ * bidirectional, lossless serial link.  
+ * See description.txt or the Wixel User's Guide for more information.
+ */
+
+/*
+ * TODO: To avoid damage, don't enable nDTR and nRTS outputs by default.
+ * TODO: use LEDs to give feedback about sending/receiving bytes.
+ * TODO: UART flow control.
+ * TODO: Obey CDC-ACM Set Line Coding commands:
+ *       In USB-RADIO mode, bauds 0-255 would correspond to radio channels.
+ * TODO: shut down radio when we are in a different serial mode
+ * TODO: make the heartbeat blinks on the Wixels be synchronized (will require
+ *       major changes to the radio_link library)
+ * TODO: turn on red LED or flash it if the Wixel is in a mode that requires USB
+ *       but has not reached the USB Configured State (this avoids the problem of
+ *       having 0 LEDs on when the Wixel is in USB-UART mode and self powered)
+ */
+
+/** Dependencies **************************************************************/
+#include <wixel.h>
+
+#include <uart1.h>
+
+
+#include "HeaderDefs.h"
+
+// #include "Commander.h"
+
+#include "dynamixel.h"
+#include "ax.h"
+
+
+/** Parameters ****************************************************************/
+#define SERIAL_MODE_AUTO        0
+#define SERIAL_MODE_USB_RADIO   1
+#define SERIAL_MODE_UART_RADIO  2
+#define SERIAL_MODE_USB_UART    3
+// int32 CODE param_serial_mode = SERIAL_MODE_AUTO;
+int32 CODE param_serial_mode = SERIAL_MODE_UART_RADIO;
+
+// This is the baud rate used by UART
+// Note: Wixel maximum UART sbaud rate is 1.5 Mbaud.
+int32 CODE param_baud_rate = DYNAMIXEL_BAUDRATE;
+
+int32 CODE param_nDTR_pin = 10;
+int32 CODE param_nRTS_pin = 11;
+int32 CODE param_nDSR_pin = 12;
+int32 CODE param_nCD_pin = 13;
+
+int32 CODE param_DTR_pin = -1;
+int32 CODE param_RTS_pin = -1;
+int32 CODE param_DSR_pin = -1;
+int32 CODE param_CD_pin = -1;
+
+int32 CODE param_arduino_DTR_pin = 0;
+
+// Approximate number of milliseconds to disable UART's receiver for after a
+// framing error is encountered.
+// Valid values are 0-250.
+// A value of 0 disables the feature (the UART's receiver will not be disabled).
+// The actual number of milliseconds that the receiver is disabled for will be
+// between param_framing_error_ms and param_framing_error_ms + 1.
+int32 CODE param_framing_error_ms = 0;
+
+/** Global Variables **********************************************************/
+
+// This bit is 1 if the UART's receiver has been disabled due to a framing error.
+// This bit should be equal to !U1CSR.RE, but we need this variable because we
+// don't want to be reading U1CSR in the main loop, because reading it might
+// cause the FE or ERR bits to be cleared and then the ISR
+// would not receive notice of those errors.
+BIT uartRxDisabled = 0;
+
+uint8 DATA currentSerialMode;
+
+BIT framingErrorActive = 0;
+
+BIT errorOccurredRecently = 0;
+uint8 lastErrorTime;
+
+/** Functions *****************************************************************/
+
+void updateLeds()
+{
+    static BIT dimYellowLed = 0;
+    static uint16 lastRadioActivityTime;
+    uint16 now;
+
+    usbShowStatusWithGreenLed();
+
+    now = (uint16)getMs();
+
+    if (currentSerialMode == SERIAL_MODE_USB_UART)
+    {
+        // The radio is not being used, so turn off the yellow LED.
+        LED_YELLOW(0);
+    }
+    else if (!radioLinkConnected())
+    {
+        // We have not connected to another device wirelessly yet, so do a
+        // 50% blink with a period of 1024 ms.
+        LED_YELLOW(now & 0x200 ? 1 : 0);
+    }
+    else
+    {
+        // We have connected.
+
+        if ((now & 0x3FF) <= 20)
+        {
+            // Do a heartbeat every 1024ms for 21ms.
+            LED_YELLOW(1);
+        }
+        else if (dimYellowLed)
+        {
+            static uint8 DATA count;
+            count++;
+            LED_YELLOW((count & 0x7)==0);
+        }
+        else
+        {
+            LED_YELLOW(0);
+        }
+    }
+
+    if (radioLinkActivityOccurred)
+    {
+        radioLinkActivityOccurred = 0;
+        dimYellowLed ^= 1;
+        //dimYellowLed = 1;
+        lastRadioActivityTime = now;
+    }
+
+    if ((uint16)(now - lastRadioActivityTime) > 32)
+    {
+        dimYellowLed = 0;
+    }
+
+    if ((uint8)(now - lastErrorTime) > 100)
+    {
+        errorOccurredRecently = 0;
+    }
+
+    LED_RED(errorOccurredRecently || uartRxDisabled);
+}
+
+/* Returns the logical values of the input control signal pins.
+   Bit 0 is DSR.
+   Bit 1 is CD. */
+uint8 ioRxSignals()
+{
+    uint8 signals = 0;
+
+    if ((param_CD_pin >= 0 && isPinHigh(param_CD_pin)) ||
+            (param_nCD_pin >= 0 && !isPinHigh(param_nCD_pin)))
+    {
+        signals |= 2;
+    }
+
+    if ((param_DSR_pin >= 0 && isPinHigh(param_DSR_pin)) ||
+            (param_nDSR_pin >= 0 && !isPinHigh(param_nDSR_pin)))
+    {
+        signals |= 1;
+    }
+
+    return signals;
+}
+
+/* Sets the logical values of the output control signal pins.
+   This should be called frequently (not just when the values change).
+   Bit 0 is DTR.
+   Bit 1 is RTS. */
+void ioTxSignals(uint8 signals)
+{
+    static uint8 nTrstPulseStartTime;
+    static uint8 lastSignals;
+
+    // Inverted outputs
+    setDigitalOutput(param_nDTR_pin, (signals & ACM_CONTROL_LINE_DTR) ? 0 : 1);
+    setDigitalOutput(param_nRTS_pin, (signals & ACM_CONTROL_LINE_RTS) ? 0 : 1);
+
+    // Non-inverted outputs.
+    setDigitalOutput(param_DTR_pin, (signals & ACM_CONTROL_LINE_DTR) ? 1 : 0);
+    setDigitalOutput(param_RTS_pin, (signals & ACM_CONTROL_LINE_RTS) ? 1 : 0);
+
+    // Arduino DTR pin.
+    if (!(lastSignals & ACM_CONTROL_LINE_DTR) && (signals & ACM_CONTROL_LINE_DTR))
+    {
+        // We just made a falling edge on the nDTR line, so start a 1-2ms high pulse
+        // on the nTRST line.
+        setDigitalOutput(param_arduino_DTR_pin, HIGH);
+        nTrstPulseStartTime = getMs();
+    }
+    else if ((uint8)(getMs() - nTrstPulseStartTime) >= 2)
+    {
+        setDigitalOutput(param_arduino_DTR_pin, LOW);
+    }
+
+    lastSignals = signals;
+}
+
+void errorOccurred()
+{
+    lastErrorTime = (uint8)getMs();
+    errorOccurredRecently = 1;
+}
+
+void errorService()
+{
+    static uint8 lastRxLowTime;
+
+    if (uart1RxBufferFullOccurred)
+    {
+        uart1RxBufferFullOccurred = 0;
+        errorOccurred();
+    }
+
+    if (uart1RxFramingErrorOccurred)
+    {
+        uart1RxFramingErrorOccurred = 0;
+
+        // A framing error occurred.
+        framingErrorActive = 1;
+        errorOccurred();
+
+        if (param_framing_error_ms > 0)
+        {
+            // Disable the UART's receiver.
+            U1CSR &= ~0x40;    // U1CSR.RE = 0.  Disables reception of bytes on the UART.
+            uartRxDisabled = 1;
+            lastRxLowTime = (uint8)getMs();  // Initialize lastRxLowTime even if the line isn't low right now.
+        }
+    }
+
+    if (framingErrorActive)
+    {
+        if (!isPinHigh(17))
+        {
+            errorOccurred();
+        }
+        else
+        {
+            framingErrorActive = 0;
+        }
+    }
+
+    if (uartRxDisabled)
+    {
+        if (!isPinHigh(17))
+        {
+            // The line is low.
+            lastRxLowTime = (uint8)getMs();
+        }
+        else if ((uint8)(getMs() - lastRxLowTime) > param_framing_error_ms)
+        {
+            // The line has been high for long enough, so re-enable the receiver.
+            U1CSR |= 0x40;
+            uartRxDisabled = 0;
+        }
+    }
+}
+
+void updateSerialMode()
+//Switches between USB/UART/radio pairings
+{
+	currentSerialMode = SERIAL_MODE_UART_RADIO;
+}
+
+
+/*
+*	Interpolate between two signed numbers
+*	value - the current value to be used
+*   minVal - the minimum that 'value' can be
+*   maxVal - the maximum that 'value' can be
+*   minRtn - the return value if 'value = minVal'
+*   maxRtn - the return value if 'value = maxVal'
+*   return a value in the range minRtn to maxRtn
+*/
+int16 interpolate(int16 value, int16 minVal, int16 maxVal, int16 minRtn, int16 maxRtn){
+	register int32  lRtnRange;
+	register int32 lValRange;
+	register int32 lRelVal;
+
+	lRtnRange = maxRtn - minRtn;
+	lValRange = maxVal - minVal;
+	lRelVal = value - minVal;
+	lRtnRange =  minRtn + ( lRtnRange * lRelVal / lValRange );
+	return (int16)lRtnRange;
+}
+
+void main()
+{
+    systemInit();
+	
+    //Among other things, allocates byte arrays for sending commands.
+    dynamixel_init();
+
+    // Oooh. what's this?
+    setDigitalOutput(param_arduino_DTR_pin, LOW);
+    ioTxSignals(0);
+
+    //usbInit();
+    uart1Init();
+    uart1SetBaudRate(param_baud_rate);
+
+    
+	
+    // Initial setting of serial mode
+    updateSerialMode();
+
+    // Set up P1_5 to be the radio's TX debug signal.
+    // P1DIR |= (1<<5);
+    // IOCFG0 = 0b011011; // P1_5 = PA_PD (TX mode)
+
+	// P1DIR |= 0x20; //Enable pin P1_5
+	
+    while(1)
+    {
+	uint32 ms;
+	uint16 now;
+	uint16 speed;
+		
+	updateSerialMode();
+	boardService();
+	updateLeds();
+	errorService();
+
+	// Code for oscillating a servo back and forth
+	ms = getMs();		// Get current time in ms
+	now = ms % (uint32)10000; 	// 10 sec for a full swing
+	if(now >= (uint16)5000){				// Goes from 0ms...5000ms
+		now = (uint16)10000 - now;			// then 5000ms...0ms
+	}
+	speed = interpolate(now, 0, 5000, 100, 900); // speed is really the position.
+	
+	ax12SetGOAL_POSITION(52, speed);
+
+	delayMs(30);
+    }
+}
