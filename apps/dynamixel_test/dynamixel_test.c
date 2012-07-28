bf2be92ced122b5b2682d628cad0efe6a341f066
erelson <gertlex@gmail.com>
2012-07-27 21:31:33 -0500

    cleanup so dynamixel_test compiles
    .

--- apps/dynamixel_test/dynamixel_test.c
+++ apps/dynamixel_test/dynamixel_test.c
@@ -88,54 +88,9 @@ void updateLeds()
     static uint16 lastRadioActivityTime;
     uint16 now;
 
-    usbShowStatusWithGreenLed();
+    // usbShowStatusWithGreenLed();
 
     now = (uint16)getMs();
-
-    if (currentSerialMode == SERIAL_MODE_USB_UART)
-    {
-        // The radio is not being used, so turn off the yellow LED.
-        LED_YELLOW(0);
-    }
-    else if (!radioLinkConnected())
-    {
-        // We have not connected to another device wirelessly yet, so do a
-        // 50% blink with a period of 1024 ms.
-        LED_YELLOW(now & 0x200 ? 1 : 0);
-    }
-    else
-    {
-        // We have connected.
-
-        if ((now & 0x3FF) <= 20)
-        {
-            // Do a heartbeat every 1024ms for 21ms.
-            LED_YELLOW(1);
-        }
-        else if (dimYellowLed)
-        {
-            static uint8 DATA count;
-            count++;
-            LED_YELLOW((count & 0x7)==0);
-        }
-        else
-        {
-            LED_YELLOW(0);
-        }
-    }
-
-    if (radioLinkActivityOccurred)
-    {
-        radioLinkActivityOccurred = 0;
-        dimYellowLed ^= 1;
-        //dimYellowLed = 1;
-        lastRadioActivityTime = now;
-    }
-
-    if ((uint16)(now - lastRadioActivityTime) > 32)
-    {
-        dimYellowLed = 0;
-    }
 
     if ((uint8)(now - lastErrorTime) > 100)
     {
