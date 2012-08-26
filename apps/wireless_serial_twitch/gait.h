334ca352d369db5280be658db0e25e3790d8bb91
Eric Relson <gertlex@gmail.com>
2012-08-26 12:39:54 -0500

    Forward walking gait initial work

--- apps/wireless_serial_twitch/gait.h
+++ apps/wireless_serial_twitch/gait.h
@@ -7,12 +7,12 @@
 // Define constants for each limb
 #define G8_LIMB_SERVO_MIDDLE 0
 #define G8_LIMB_SERVO_RIGHT 1
-#define G8_LIMB_SERVO_LEF 2
+#define G8_LIMB_SERVO_LEFT 2
 
 // Define constants for each animation sequence
 #define G8_ANIM_DEFAULT 0
 #define G8_ANIM_START 1
-#define G8_ANIM_DEFAULT2 2
+#define G8_ANIM_WALK_STRAIGHT 2
 
 
 // The limb positions for animation number 0 Frame#0
@@ -52,16 +52,58 @@ static const  G8_LIMB_POSITION  limbs_1_1[G8_NUM_LIMBS] = {
 
 // The limb positions for animation number 2 Frame#0
 static const  G8_LIMB_POSITION  limbs_2_0[G8_NUM_LIMBS] = {
-	MAKE_G8_LIMB_POSITION( -96,102 , 144,-153 , 444,0 , 51 ),
-	MAKE_G8_LIMB_POSITION( -102,0 , 153,0 , 441,0 , 0 ),
-	MAKE_G8_LIMB_POSITION( -102,0 , 153,0 , 441,0 , 0 )
+	MAKE_G8_LIMB_POSITION( -60,0 , 90,0 , 123,0 , -51 ),
+	MAKE_G8_LIMB_POSITION( -57,-21 , 87,30 , 123,42 , 0 ),
+	MAKE_G8_LIMB_POSITION( -57,-21 , 87,30 , 123,42 , 0 )
 };
 
 // The limb positions for animation number 2 Frame#1
 static const  G8_LIMB_POSITION  limbs_2_1[G8_NUM_LIMBS] = {
-	MAKE_G8_LIMB_POSITION( -98,-102 , 147,153 , 459,0 , 0 ),
-	MAKE_G8_LIMB_POSITION( -98,0 , 147,0 , 459,0 , 0 ),
-	MAKE_G8_LIMB_POSITION( -98,0 , 147,0 , 459,0 , 0 )
+	MAKE_G8_LIMB_POSITION( -23,-63 , 30,114 , 87,0 , -51 ),
+	MAKE_G8_LIMB_POSITION( -35,-28 , 54,30 , 75,24 , 51 ),
+	MAKE_G8_LIMB_POSITION( -35,-26 , 54,27 , 75,24 , 51 )
+};
+
+// The limb positions for animation number 2 Frame#2
+static const  G8_LIMB_POSITION  limbs_2_2[G8_NUM_LIMBS] = {
+	MAKE_G8_LIMB_POSITION( -31,-60 , 51,69 , 84,42 , 0 ),
+	MAKE_G8_LIMB_POSITION( -37,30 , 54,-57 , 87,0 , 77 ),
+	MAKE_G8_LIMB_POSITION( -37,26 , 54,-51 , 87,0 , 76 )
+};
+
+// The limb positions for animation number 2 Frame#3
+static const  G8_LIMB_POSITION  limbs_2_3[G8_NUM_LIMBS] = {
+	MAKE_G8_LIMB_POSITION( -58,0 , 87,0 , 120,0 , 51 ),
+	MAKE_G8_LIMB_POSITION( -55,25 , 81,-39 , 123,-36 , 50 ),
+	MAKE_G8_LIMB_POSITION( -55,24 , 81,-39 , 123,-36 , 51 )
+};
+
+// The limb positions for animation number 2 Frame#4
+static const  G8_LIMB_POSITION  limbs_2_4[G8_NUM_LIMBS] = {
+	MAKE_G8_LIMB_POSITION( -54,0 , 81,0 , 123,0 , 51 ),
+	MAKE_G8_LIMB_POSITION( -54,24 , 81,-33 , 123,-42 , 0 ),
+	MAKE_G8_LIMB_POSITION( -57,24 , 87,-33 , 120,-42 , 0 )
+};
+
+// The limb positions for animation number 2 Frame#5
+static const  G8_LIMB_POSITION  limbs_2_5[G8_NUM_LIMBS] = {
+	MAKE_G8_LIMB_POSITION( -29,60 , 39,-111 , 90,0 , 51 ),
+	MAKE_G8_LIMB_POSITION( -35,26 , 54,-27 , 81,-24 , -51 ),
+	MAKE_G8_LIMB_POSITION( -35,28 , 54,-30 , 81,-24 , -51 )
+};
+
+// The limb positions for animation number 2 Frame#6
+static const  G8_LIMB_POSITION  limbs_2_6[G8_NUM_LIMBS] = {
+	MAKE_G8_LIMB_POSITION( -25,63 , 42,-75 , 78,-39 , 0 ),
+	MAKE_G8_LIMB_POSITION( -37,-26 , 54,51 , 78,0 , -76 ),
+	MAKE_G8_LIMB_POSITION( -37,-28 , 54,54 , 78,0 , -77 )
+};
+
+// The limb positions for animation number 2 Frame#7
+static const  G8_LIMB_POSITION  limbs_2_7[G8_NUM_LIMBS] = {
+	MAKE_G8_LIMB_POSITION( -58,0 , 87,0 , 126,0 , -51 ),
+	MAKE_G8_LIMB_POSITION( -58,-21 , 87,33 , 126,39 , -51 ),
+	MAKE_G8_LIMB_POSITION( -58,-21 , 87,33 , 126,39 , -51 )
 };
 
 // The frames for animation number 0
@@ -78,12 +120,18 @@ static const  G8_FRAME  frames_1[2] = {
 };
 
 // The frames for animation number 2
-static const  G8_FRAME  frames_2[2] = {
+static const  G8_FRAME  frames_2[8] = {
 	MAKE_G8_FRAME(0, limbs_2_0),
-	MAKE_G8_FRAME(492, limbs_2_1)
+	MAKE_G8_FRAME(153, limbs_2_1),
+	MAKE_G8_FRAME(247, limbs_2_2),
+	MAKE_G8_FRAME(351, limbs_2_3),
+	MAKE_G8_FRAME(500, limbs_2_4),
+	MAKE_G8_FRAME(650, limbs_2_5),
+	MAKE_G8_FRAME(750, limbs_2_6),
+	MAKE_G8_FRAME(845, limbs_2_7)
 };
 const  G8_ANIMATION  animations[3] = {
 	MAKE_G8_ANIMATION(3, frames_0, FALSE),
 	MAKE_G8_ANIMATION(2, frames_1, TRUE),
-	MAKE_G8_ANIMATION(2, frames_2, FALSE)
+	MAKE_G8_ANIMATION(8, frames_2, FALSE)
 };
