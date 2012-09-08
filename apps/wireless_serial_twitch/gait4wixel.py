34edeb785c23aade5411cf6c1c6999fa20064995
Eric Relson <gertlex@gmail.com>
2012-09-08 17:29:14 -0500

    Corrected turning

--- apps/wireless_serial_twitch/gait4wixel.py
+++ apps/wireless_serial_twitch/gait4wixel.py
@@ -31,5 +31,5 @@ if (not fileAtt & stat.S_IWRITE):
 replace(myFile, '<Gait/GaitRunner.h>', '"GaitRunner.h"')
 replace(myFile, 'PROGMEM', '')
 # replace(myFile, 'MAKE_G8_ANIMATION(2, frames_2, TRUE),', 'MAKE_G8_ANIMATION(2, frames_2, 2),')
-replace(myFile, 'MAKE_G8_ANIMATION(2, frames_1, TRUE),', 'MAKE_G8_ANIMATION(2, frames_1, 2),')
+# replace(myFile, 'MAKE_G8_ANIMATION(2, frames_1, TRUE),', 'MAKE_G8_ANIMATION(2, frames_1, 2),')
 replace(myFile, 'MAKE_G8_ANIMATION(2, frames_3, TRUE),', 'MAKE_G8_ANIMATION(2, frames_3, 2),')
